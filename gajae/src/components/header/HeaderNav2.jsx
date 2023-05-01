1912e3792a779b2169114a9af02a3dc115917800
WungCoderProject <wungproject@gmail.com>
2023-05-01 21:04:27 +0900

    real

--- gajae/src/components/header/HeaderNav2.jsx
+++ gajae/src/components/header/HeaderNav2.jsx
@@ -6,32 +6,34 @@ import '../main/mainSearchBar.css';
 const HeaderNav2 = () => {
   return (
     <>
-      <div className="header">
-        <div className="headerContainer listMode">
-          <div className="headerList">
-            <div className="headerListItem ">
-              <FontAwesomeIcon icon={faBed} />
-              <span>숙 소</span>
-            </div>
-            <div className="headerListItem">
-              <FontAwesomeIcon icon={faPlane} />
-              <span>항 공 권</span>
-            </div>
-            <div className="headerListItem">
-              <FontAwesomeIcon icon={faCar} />
-              <span>렌 터 카</span>
-            </div>
-            <div className="headerListItem">
-              <FontAwesomeIcon icon={faBed} />
-              <span>투 어</span>
-            </div>
-            <div className="headerListItem">
-              <FontAwesomeIcon icon={faTaxi} />
-              <span>택 시</span>
-            </div>
-          </div>
-        </div>
+<header>
+<div className="headerContainer listMode" >
+    <div className="headerList">
+      <div className="headerListItem">
+        <FontAwesomeIcon icon={faBed} />
+        <span>숙 소</span>
       </div>
+      <div className="headerListItem">
+        <FontAwesomeIcon icon={faPlane} />
+        <span>항 공 권</span>
+      </div>
+      <div className="headerListItem">
+        <FontAwesomeIcon icon={faCar} />
+        <span>렌 터 카</span>
+      </div>
+      <div className="headerListItem">
+        <FontAwesomeIcon icon={faBed} />
+        <span>투 어</span>
+      </div>
+      <div className="headerListItem">
+        <FontAwesomeIcon icon={faTaxi} />
+        <span>택 시</span>
+      </div>
+    </div>
+  </div>
+</header>
+
+  
     </>
   );
 };
